c60986bb8da8cf70221cec66fdb070f9a77f0566
ryan connolly <connolly-r16@ulster.ac.uk>
2022-08-15 21:18:16 +0100

    Add working login system

--- src/js/login.js
+++ src/js/login.js
@@ -2,24 +2,37 @@ const LOGIN_BUTTON = document.getElementById("loginButton");
 var USERNAME_TXT = document.getElementById("usernameTxt");
 var PASSWORD_TXT = document.getElementById("passwordTxt");
 
-
 LOGIN_BUTTON.onclick = function checkCredentials() {
-    //LOGIN_BUTTON.style.backgroundColor = 'yellow';
-    //LOGIN_BUTTON.style.color = 'blue';
 
-     let username = USERNAME_TXT.value;
-     let password = PASSWORD_TXT.value;
+    var username = USERNAME_TXT.value;
+    var password = PASSWORD_TXT.value;
 
-    const formData = new FormData();
+    //Create Object and convert to JSON
+    var data = {
+        userName: username,
+        password: password,
+      };
+    body = JSON.stringify(data);
 
-    formData.append("userName", username);
-    formData.append("password", password);
 
-    
+    //Open request and send JSON Body
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://prod-253.westeurope.logic.azure.com:443/workflows/1aa36e7d5f2c4effb59df6ebdf0d4cda/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FN0tr_TEahENkKe4fQSxoaMPkmaXVweqkn63sXrTdMU");
+    //request.setRequestHeader("Accept", "application/json");
+    request.setRequestHeader("Content-Type", "application/json");
 
-    //  const request = new 
-    //  XMLHttpRequest();
-    //  request.open("POST", "www.logicAppURL.com");
-    //  request.send(formData);
+    request.send(body);
+
+    request.onreadystatechange = (e) => {
+        if(request.responseText == "\"matchDetermined\":true"){
+            document.location='/src/html/loops.html';
+        }else{
+            USERNAME_TXT.style.borderBlockColor = 'red';
+            PASSWORD_TXT.style.borderBlockColor = 'red';
+
+        }
+
+   }
+
 }
 
