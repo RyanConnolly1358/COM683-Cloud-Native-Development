db232d0f6e2e83aed9b041e0a844cde0ebd8fda6
ryan connolly <connolly-r16@ulster.ac.uk>
2022-08-17 00:03:18 +0100

    Add Sign-Up System

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -8,7 +8,35 @@ SIGNUP_BUTTON.onclick = function(){
     // SIGNUP_BUTTON.innerHTML = USERNAME_TXT.value + PASSWORD_TXT.value
     if(PASSWORD_TXT.value == CONFIRM_PASSWORD_TXT.value && PASSWORD_TXT.value != ""){
         //Update record in database
-        SIGNUP_BUTTON.innerHTML = "true";
+        //Create Object and convert to JSON
+    var data = {
+        userName: USERNAME_TXT.value,
+        password: PASSWORD_TXT.value,
+      };
+    body = JSON.stringify(data);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://prod-154.westeurope.logic.azure.com:443/workflows/62b6919ee4c1412fb7368f549be3fd9b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=bzuyWL66e2MHD7ahgjkPt3pNyY37W1fKjCE8qIHC79c");
+    //request.setRequestHeader("Accept", "application/json");
+    request.setRequestHeader("Content-Type", "application/json");
+    
+    request.send(body);
+
+    request.onreadystatechange = (e) => {
+        if(request.responseText == "\"accountCreated\": true"){
+            document.location='/src/html/loops.html';
+        }else{
+            SIGNUP_BUTTON.innerHTML = request.responseText;
+            USERNAME_TXT.style.borderBlockColor = 'red';
+            PASSWORD_LBL.innerHTML = "Username already taken";
+            PASSWORD_LBL.hidden = false;
+            PASSWORD_LBL.style.color = 'red';
+
+        }
+
+   }
+
+
     }else{
         //Highlight issue.
         CONFIRM_PASSWORD_TXT.style.borderBlockColor = 'red'
