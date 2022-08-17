3a789d640e4025901c55687bb3bd32baecf4a93d
ryan connolly <connolly-r16@ulster.ac.uk>
2022-08-17 19:28:09 +0100

    Add upload picture to blob

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,4 +1,7 @@
 let dropArea = document.getElementById('drop-area')
+const SUBMIT_BUTTON = document.getElementById('submitBtn')
+const inputFile = document.getElementById('fileElem')
+var img = document.createElement('img')
 
 ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, preventDefaults, false)
@@ -15,7 +18,7 @@ dropArea.addEventListener('dragover', highlight)
 dropArea.addEventListener('drop', unhighlight)
 dropArea.addEventListener('drop', fileDropped)
 
-  
+
 function highlight(e) {
     dropArea.classList.add('highlight')
 }
@@ -28,15 +31,41 @@ function unhighlight(e) {
 function fileDropped(e) {
 
     const file = e.dataTransfer.files[0];
+    inputFile.files = e.dataTransfer.files;
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.addEventListener('loadend', () =>{
-        var img = document.createElement('img')
         img.src = reader.result;
-        document.getElementById('preview').append(img)
+        document.getElementById('preview').append(img);
     })
     
 }
+
+SUBMIT_BUTTON.onclick = function(){
+    event.preventDefault();
+    
+    if(img.src != ""){
+
+        const formData = new FormData();
+        
+        for (const file of inputFile.files) {
+            formData.append("files", file);
+            
+        }
+            
+            fetch("https://createimageblob.azurewebsites.net/api/uploadImage?", {
+                method: "post",
+                body: formData,
+            }).catch((error) => ("Something went wrong!", error));
+
+    }else{
+        SUBMIT_BUTTON.innerHTML = "Empty"
+    }
+
+}
+
+
+
 
 
   
